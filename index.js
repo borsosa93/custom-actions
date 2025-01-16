431d12c800739c556ba5a507470d10f4092f3d69
borsosa93 <79048204+borsosa93@users.noreply.github.com>
2025-01-16 17:58:52 +0000

    initial version of the action

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 import * as core from '@actions/core';
+import * as core from '@actions/github';
 
-const name = core.getInput('name');
-const outputVal=`Hello ${name}!`;
-core.setOutput('greeting', 'outputVal');
+try{
+    const name = core.getInput('name');
+    const outputVal=`Hello ${name}!`;
+    core.setOutput('greeting', 'outputVal');
+} catch(error){
+    core.setFailed(error.message);
+}
+
